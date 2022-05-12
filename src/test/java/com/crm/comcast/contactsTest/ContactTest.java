545c55e01fdcc8d7f14541318e506061f73b516c
Deeparpee <103677894+Deeparpee@users.noreply.github.com>
2022-05-12 10:59:23 +0530

    Update ContactTest.java

--- src/test/java/com/crm/comcast/contactsTest/ContactTest.java
+++ src/test/java/com/crm/comcast/contactsTest/ContactTest.java
@@ -9,15 +9,12 @@ public class ContactTest
   {
 	  System.out.println("===Contact is created=="+System.getProperty("browser"));
 	 
-  }  
-   //Update
+  } 
   @Test(groups = "RegressionTest")
   public void editcontactTest() 
   {
 	  System.out.println("===Contact is edited==");
   }
-  
-  //Test case is updated
   @Test(groups = "RegressionTest")
   public void deletecontactTest()
   {
